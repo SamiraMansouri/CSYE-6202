d12092a83fc42a0962c5ddbaf48c3c9949812f79
SamiraMansouri. <mansourimajomerd.s@husky.neu.edu>
2015-09-29 17:01:39 -0400

    First Check in

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -5,10 +5,51 @@
 		public long FindSumOfPrimeNumbers(int n)
 		{
 			long sum = 0;
+            
+            int count = 0;
+            int num = 0;
 
-			// your solution goes in here
-
-			return sum;
+            while (count < n)
+            {
+                if (IsPrime(num))
+                {
+                    sum += num;
+                    count++;
+                }
+                num++;
+            }
+            
+            return sum;
 		}
-	}
+
+        private bool IsPrime(int Num)
+        {
+            bool result = true;
+            if (Num < 2)
+            {
+                result = false;
+            }
+            else if (Num == 2)
+            {
+                result = true;
+            }
+            else if (Num % 2 == 0)
+            {
+                result = false;
+            }
+            else if ((Num % 2) != 0)
+            {
+                for (int i = 2; i < Num; i++)
+                {
+                    if (Num % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+
+                }
+            }
+            return result;
+        }
+    }
 }
--- FindingPrimeNumbers/Program.cs
+++ FindingPrimeNumbers/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace FindingPrimeNumbers
+﻿using System;
+using FindingPrimeNumbers;
+namespace FindingPrimeNumbers
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+            Console.WriteLine("Please Enter Your Number:");
+            int Num =Convert.ToInt32( Console.ReadLine());
+            FindingPrimeNumbers Prime = new FindingPrimeNumbers();
+            long Sum=Prime.FindSumOfPrimeNumbers(Num);
+            Console.WriteLine("The Sum IS:"+Sum.ToString());
+            Console.ReadLine();
 		}
 	}
 }
