414a5a5ae47c5656335d7adc67ca763f7e4d0efd
SamiraMansouri. <mansourimajomerd.s@husky.neu.edu>
2015-09-30 18:48:41 -0400

    First Check in:

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -16,22 +16,67 @@ namespace TrianglePatterns
 
 		static void DisplayPatternA()
 		{
-			// your implementation here
-		}
+            Console.Write("Pattern A is: "+'\n');
+            for (int i = 0; i < 10 ; i++)
+            {
+                for (int j = 0; j < (i+1); j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+        }
 
 		static void DisplayPatternB()
 		{
-			// your implementation here
-		}
+            Console.Write("Pattern B is: " + '\n');
+            for (int i = 11; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+        }
 
 		static void DisplayPatternC()
 		{
-			// your implementation here
-		}
+            Console.Write("Pattern C is: " + '\n');
+            for (int i = 11; i > 0; i--)
+            {
+                for (int k = (11 - i); k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = (i-1); j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+               
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+        }
 
 		static void DisplayPatternD()
 		{
-			// your implementation here
-		}
+            Console.Write("Pattern D is: " + '\n');
+            for (int i = 11; i > 0; i--)
+            {
+                for (int j = (i - 1); j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = (11 - i); k > 0; k--)
+                {
+                    Console.Write("*");
+                }
+                Console.Write('\n');
+            }
+            Console.Write('\n');
+        }
 	}
 }
